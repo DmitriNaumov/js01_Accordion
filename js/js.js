01588279d5182a5160fd28e25a134bf97628c427
DmitriNaumov <dmitr9628@gmail.com>
2022-07-04 17:43:33 +0300

    del row

--- js/js.js
+++ js/js.js
@@ -23,29 +23,10 @@ $(function () {
     function oneItemAckordion() {
         $('.accordion_one_item .accordion__item').on('click', function () {
             const timeAnim = 250;
-            // $(this).toggleClass('active');
-            // $(this).find('.accordion__header').next().slideToggle(timeAnim);
-            // $(this).css({ 'pointer-events': 'none' });
-
             $('.accordion_one_item .accordion__item').removeClass('active').css({ 'pointer-events': 'auto' });
             $(this).addClass('active').css({ 'pointer-events': 'none' });
             $('.accordion_one_item .accordion__header').next().slideUp(timeAnim);
             $(this).find('.accordion__header').next().slideDown(timeAnim);
-            // setTimeout(function () {
-            //     $(this).css({ 'pointer-events': 'auto' });
-            // }.bind(this), timeAnim);
-
-
-            // let topItem = $('.top__item');
-            // topItem.on('click', function () {
-            //     let timeAnim = 250;
-            //     $('.top__item').removeClass('active');
-            //     $(this).addClass('active');
-            //     $('.top__head').next().slideUp(timeAnim);
-            //     $(this).find('.top__head').next().slideDown(timeAnim);
-            // });
-
-
         });
     }
     oneItemAckordion();
